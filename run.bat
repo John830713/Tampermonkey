b8d3ee7a0a024412c744684c27e38cf7b8191a83
John_SW_Wang <john_sw_wang@wistron.com>
2026-07-15 22:23:32 +0800

    fix: correct tray.py path in run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 cd /d "%~dp0"
 for /f "tokens=*" %%i in ('powershell -NoProfile -Command "(Get-Content server_config.json | ConvertFrom-Json).show_console"') do set CONSOLE=%%i
 if "%CONSOLE%"=="False" (
-    start "" pythonw resources\tools\tray.py %*
+    start "" pythonw resources\tools\local\tray.py %*
 ) else (
-    python resources\tools\tray.py %*
+    python resources\tools\local\tray.py %*
 )
