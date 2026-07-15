3e4eade898c102d6f227dab194084804e94efab4
John_SW_Wang <john_sw_wang@wistron.com>
2026-07-15 15:37:24 +0800

    feat: consolidate run.bat with show_console config, remove run-hidden.bat

--- run-hidden.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start "" pythonw resources\tools\tray.py %*
--- run.bat
+++ run.bat
@@ -1,3 +1,8 @@
 @echo off
 cd /d "%~dp0"
-python resources\tools\tray.py %*
+for /f "tokens=*" %%i in ('powershell -NoProfile -Command "(Get-Content server_config.json | ConvertFrom-Json).show_console"') do set CONSOLE=%%i
+if "%CONSOLE%"=="False" (
+    start "" pythonw resources\tools\tray.py %*
+) else (
+    python resources\tools\tray.py %*
+)
